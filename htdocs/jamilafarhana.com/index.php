49c06f50416bd57f6c48d2d2db730e989e3e8dc5
mila155 <145261092+mila155@users.noreply.github.com>
2025-03-26 00:08:12 +0700

    Add files via upload

---
+++ htdocs/jamilafarhana.com/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body> 
+    <?php include "layout/header.html" ?>
+
+    <main>
+        <p>Halo Selamat Datang di Website Saya</p>
+    </main>
+
+    <?php include "layout/footer.html" ?>
+</body>
+</html>
